0dcde33cb6e9393a38d4310108321e6862a1414c
1996szw <1445427773@qq.com>
2018-09-03 16:21:29 +0800

    like component init

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,18 +1,25 @@
 // pages/classic.js
+import {ClassicModel} from '../../models/classic.js'
+let classic = new ClassicModel()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    classic: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    classic.getLatest((res) => {
+      this.setData({
+        classic: res
+      })
+    })
   },
 
   /**
